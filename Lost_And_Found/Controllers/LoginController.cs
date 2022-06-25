ea129995910736d28c4018775508510b7ddbd1a0
Tayyab Irfan <tayyabirfan7272@gmail.com>
2022-06-26 02:54:44 +0500

    Initial Push Axib 26-6-2022

---
+++ Lost_And_Found/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lost_And_Found.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+    }
+}
