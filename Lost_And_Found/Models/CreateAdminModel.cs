e1d5ee88b482690ff41216e0891561b1ff9325c0
Muhammad Azib Ali <muhammadazibali20@gmail.com>
2022-06-27 00:30:19 +0500

    model binding axib

--- Lost_And_Found/Models/CreateAdminModel.cs
+++ Lost_And_Found/Models/CreateAdminModel.cs
@@ -11,6 +11,7 @@ namespace Lost_And_Found.Models
         public string Admin_FullName { get; set; }
         public string Admin_Email { get; set; }
         public string Admin_Password { get; set; }
+        public long Admin_Contact { get; set; }
         public string Admin_Picture { get; set; }
         public string Admin_Role { get; set; }
         public System.DateTime Admin_Created_ON { get; set; }
@@ -18,6 +19,5 @@ namespace Lost_And_Found.Models
         public System.DateTime Admin_Updated_ON { get; set; }
         public System.DateTime Admin_Updated_By { get; set; }
         public bool Admin_IsActive { get; set; }
-        public long Admin_Contact { get; set; }
     }
 }
