0687243fc6185bc5f195abec7a19f252d02698cf
atiqakhan2205 <atiqakhan2205@gmail.com>
2022-06-27 23:53:33 +0500

    Atiqa All cards done

--- Lost_And_Found/Lost_And_Found/Controllers/CategoryController.cs
+++ Lost_And_Found/Lost_And_Found/Controllers/CategoryController.cs
@@ -3,50 +3,202 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lost_And_Found.Models;
 
 namespace Lost_And_Found.Controllers
 {
     public class CategoryController : Controller
     {
-        public ActionResult Mobile()
+        public ActionResult Mobile(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult Pets()
+
+        public ActionResult Pets(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult Cars()
+        public ActionResult Cars(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult Clothes_Shoes()
+        public ActionResult Clothes_Shoes(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult Bags()
+        public ActionResult Bags(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult People()
+        public ActionResult People(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult Documents()
+        public ActionResult Documents(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj); ;
         }
-        public ActionResult Laptop()
+        public ActionResult Laptop(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult Jewellery()
+        public ActionResult Jewellery(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
-        public ActionResult Keys()
+        public ActionResult Keys(CategoryModel obj)
         {
-            return View();
+            List<CategoryModel> lobj = new List<CategoryModel>();
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 1";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            obj = new CategoryModel();
+            obj.title = "Card Layout 2";
+            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
+            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
+            lobj.Add(obj);
+
+            lobj.Add(obj);
+            return View(lobj);
         }
     }
 }
