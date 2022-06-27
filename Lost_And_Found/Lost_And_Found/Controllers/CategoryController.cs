6fef61cbf5c0dbd6b922085f76f7cccfd3626ae9
Azib <Azib@IMRANSAEED>
2022-06-27 17:47:06 +0500

    Add CategoryController and its Views Axib

---
+++ Lost_And_Found/Lost_And_Found/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lost_And_Found.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ActionResult Mobile()
+        {
+            return View();
+        }
+        public ActionResult Pets()
+        {
+            return View();
+        }
+        public ActionResult Cars()
+        {
+            return View();
+        }
+        public ActionResult Clothes_Shoes()
+        {
+            return View();
+        }
+        public ActionResult Bags()
+        {
+            return View();
+        }
+        public ActionResult People()
+        {
+            return View();
+        }
+        public ActionResult Documents()
+        {
+            return View();
+        }
+        public ActionResult Laptop()
+        {
+            return View();
+        }
+        public ActionResult Jewellery()
+        {
+            return View();
+        }
+        public ActionResult Keys()
+        {
+            return View();
+        }
+    }
+}
