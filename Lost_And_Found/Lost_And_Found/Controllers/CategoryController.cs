fd7e4bdba6e0019be3458391ffa3bc2f7c6c0ccf
atiqakhan2205 <atiqakhan2205@gmail.com>
2022-07-01 03:14:40 +0500

    Category work by atiqa

--- Lost_And_Found/Lost_And_Found/Controllers/CategoryController.cs
+++ Lost_And_Found/Lost_And_Found/Controllers/CategoryController.cs
@@ -4,201 +4,81 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Lost_And_Found.Models;
+using Lost_And_Found.Manager;
 
 namespace Lost_And_Found.Controllers
 {
     public class CategoryController : Controller
     {
-        public ActionResult Mobile(CategoryModel obj)
+        public ActionResult Mobile(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> mobilelist = obj.selectmobiles();
+            return View(mobilelist);
         }
 
-        public ActionResult Pets(CategoryModel obj)
+        public ActionResult Pets(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> petslist = obj.selectpets();
+            return View(petslist);
         }
-        public ActionResult Cars(CategoryModel obj)
+
+        public ActionResult Cars(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> carslist = obj.selectcars();
+            return View(carslist);
         }
-        public ActionResult Clothes_Shoes(CategoryModel obj)
+
+        public ActionResult Clothes_Shoes(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> cslist = obj.selectclothes_shoes();
+            return View(cslist);
         }
-        public ActionResult Bags(CategoryModel obj)
+
+        public ActionResult Bags(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> bagslist = obj.selectbags();
+            return View(bagslist);
         }
-        public ActionResult People(CategoryModel obj)
+
+        public ActionResult People(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> peoplelist = obj.selectpeople();
+            return View(peoplelist);
         }
-        public ActionResult Documents(CategoryModel obj)
+
+        public ActionResult Documents(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj); ;
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> docslist = obj.selectdocuments();
+            return View(docslist);
         }
-        public ActionResult Laptop(CategoryModel obj)
+
+        public ActionResult Laptop(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> laptoplist = obj.selectlaptop();
+            return View(laptoplist);
         }
-        public ActionResult Jewellery(CategoryModel obj)
+
+        public ActionResult Jewellery(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> jewellerylist = obj.selectjewellery();
+            return View(jewellerylist);
         }
-        public ActionResult Keys(CategoryModel obj)
+
+
+        public ActionResult Keys(PostProductModel ppm)
         {
-            List<CategoryModel> lobj = new List<CategoryModel>();
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 1";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            obj = new CategoryModel();
-            obj.title = "Card Layout 2";
-            obj.text = "Hello everyone this videos I want to show how to use card bootstrap 4 with data binding source.";
-            obj.img = "https://library.kissclipart.com/20181214/xe/kissclipart-red-colour-clipart-mobile-phones-computer-icons-8910d5a4d3ffa5e2.png";
-            lobj.Add(obj);
-
-            lobj.Add(obj);
-            return View(lobj);
+            CategoryManager obj = new CategoryManager();
+            List<PostProductModel> keyslist = obj.selectKeys();
+            return View(keyslist);
         }
     }
 }
